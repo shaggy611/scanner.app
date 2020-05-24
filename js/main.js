5d63c95c16747d0b78bd7eb5b98a3914118a42f2
shaggy611 <sevczik11@gmail.com>
2020-05-24 19:36:51 +0300

    desktop

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  // let connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+  // console.log(connection);
+  // document.write(connection.effectiveType);
+});  
