323f33b9bba00494f1d420dd29e3c3a916dafb2f
shaggy611 <sevczik11@gmail.com>
2020-06-05 14:31:44 +0300

    bug

--- js/main.js
+++ js/main.js
@@ -98,66 +98,63 @@ $(document).ready(function () {
       how_works_arrow.show();
     }
 
-    if ($(window).scrollTop() > 1450) {
-      features_arrow.addClass("_appear_dots");
-      features_arrow.show();
-        // SLIDER INITIALIZATION==================================================
-  $(".js-slider").slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-  // ===========================================================================
-    }
-
-    if ($(window).scrollTop() > 2301) {
+    if ($(window).scrollTop() > 2201) {
       testimonials_arrow.addClass("_appear_dots");
       testimonials_arrow.show()
     }
+
+    if ($(window).scrollTop() > 1350) {
+      features_arrow.addClass("_appear_dots");
+      features_arrow.show();
+        // SLIDER INITIALIZATION==================================================
+                $(".js-slider").slick({
+                  dots: true,
+                  arrows: false,
+                  infinite: true,
+                  speed: 300,
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  autoplay: true,
+                  autoplaySpeed: 3000,
+                  responsive: [
+                    {
+                      breakpoint: 1200,
+                      settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                      }
+                    },
+                    {
+                      breakpoint: 1024,
+                      settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                      }
+                    },
+                    {
+                      breakpoint: 767,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true
+                      }
+                    },
+                    {
+                      breakpoint: 480,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true
+                      }
+                    }
+                  ]
+                });
+  // ===========================================================================
+    };
 
   });
   // ===========================================================================
