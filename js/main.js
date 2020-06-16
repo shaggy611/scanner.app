2beefc6ddb8cdbcf53801c7b3d41c5ba96b9bfd8
shaggy611 <sevczik11@gmail.com>
2020-06-16 16:48:25 +0300

    bug

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1200,
